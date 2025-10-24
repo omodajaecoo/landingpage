58eec660a0614b5c2ee3256eb09b32143a10bd71
omodajaecoo <omodajaecoo@users.noreply.github.com>
2025-10-24 15:15:50 +0000

    sync: actualización automática desde automotores-front

--- destino/destino/destino/destino/destino/destino/destino/destino/components/home/formInfo.vue
+++ destino/destino/destino/destino/destino/destino/destino/destino/components/home/formInfo.vue
@@ -1,52 +1,91 @@
 <template>
-    <div
-        class="w-full flex bg-[#0D0E0E] justify-center items-center min-h-[100px] pb-[0px] p-[10px] pt-[32px] sm:pt-[100px]"
-      >
-        <div class="w-[800px] max-w-[90%] flex flex-col items-center">
-          
-          <div class="w-full flex flex-col items-center text-center mb-[40px] sm:pb-[0px]">
-            <h1 class="font-inter font-normal text-[24px] leading-[28px] sm:text-[32px] sm:leading-[38px] text-white sm:mx-[113px]">
-              LLENA EL SIGUIENTE FORMULARIO Y SE PARTE DE LOS EARLY ADOPTERS
-            </h1>
+  <div class="w-full flex bg-[#0D0E0E] justify-center items-center min-h-[100px] sm:px-[10px] pt-[32px] sm:pt-[100px] pb-[40px]">
+    <div class="w-[900px] max-w-[90%] flex flex-col justify-center items-center space-y-[32px]">
+      <h1 class="text-center font-interRegular text-[24px] leading-[28px] sm:text-[32px] sm:leading-[38px] text-white sm:w-[600px]">
+        LLENA EL SIGUIENTE FORMULARIO Y SE PARTE DE LOS EARLY ADOPTERS
+      </h1>
 
-            <div class="my-[32px] bg-[#67B0C480] rounded-[8px] p-2 flex flex-col items-center mx-auto sm:w-[300px]">
-              <div class="flex justify-center items-center gap-x-[8px] w-full">
-                <div class="flex flex-col items-center w-[22px]">
-                  <span class="font-inter text-[15px] font-semibold text-white">{{ countdown.days }}</span>
-                  <span class="font-inter text-[7px] text-white">DÍAS</span>
-                </div>
-                <span class="font-inter text-[15px] font-semibold text-white">:</span>
-                <div class="flex flex-col items-center w-[22px]">
-                  <span class="font-inter text-[15px] font-semibold text-white">{{ countdown.hours }}</span>
-                  <span class="font-inter text-[7px] text-white">HORAS</span>
-                </div>
-                <span class="font-inter text-[15px] font-semibold text-white">:</span>
-                <div class="flex flex-col items-center w-[22px]">
-                  <span class="font-inter text-[15px] font-semibold text-white">{{ countdown.minutes }}</span>
-                  <span class="font-inter text-[7px] text-white">MIN</span>
-                </div>
-                <span class="font-inter text-[15px] font-semibold text-white">:</span>
-                <div class="flex flex-col items-center w-[22px]">
-                  <span class="font-inter text-[15px] font-semibold text-white">{{ countdown.seconds }}</span>
-                  <span class="font-inter text-[7px] text-white">SEG</span>
-                </div>
-              </div>
-              <span class="mt-[10px] font-inter text-white font-semibold text-[12px]">Preventa oficial en Ecuador</span>
-            </div>
-
-            <p class="mb-[32px] font-inter text-[14px] sm:text-[16px] text-white font-light text-center whitespace-pre-line">
-              Rediseñamos la experiencia, y tú puedes ser parte de ella.
-            </p>
-            <p class="font-inter text-[14px] sm:text-[16px] text-white font-light text-center whitespace-pre-line">
-              Para los early adopters, ofrecemos beneficios exclusivos, challenges y sorteos que convierten cada momento al volante en algo único y emocionante. Vive lo que otros solo imaginan y descubre cómo es rediseñar lo convencional.
-            </p>
+      <div class="w-full flex justify-center space-x-[10px] sm:space-x-[14px] md:space-x-[24px] lg:space-x-[32px]">
+        <div class="w-[60px] h-[70px] sm:w-[120px] sm:h-[120px] lg:w-[160px] lg:h-[160px] p-[16px] sm:p-[24px] lg:p-[32px] bg-[#67B0C480] rounded-[8px] flex flex-col justify-center items-center flex-shrink-0">
+          <div class="flex flex-col items-center space-y-[8px]">
+            <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+              {{ countdown.days }}
+            </span>
+            <span class="font-interSemiBold text-[10px] sm:text-[12px] lg:text-[16px] text-white text-center leading-none mt-1">
+              DÍAS
+            </span>
           </div>
-    
         </div>
+
+        <div class="flex flex-col justify-center items-center">
+          <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+            :
+          </span>
+        </div>
+
+        <div class="w-[60px] h-[70px] sm:w-[120px] sm:h-[120px] lg:w-[160px] lg:h-[160px] p-[16px] sm:p-[24px] lg:p-[32px] bg-[#67B0C480] rounded-[8px] flex flex-col justify-center items-center flex-shrink-0">
+          <div class="flex flex-col items-center space-y-[8px]">
+            <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+              {{ countdown.hours }}
+            </span>
+            <span class="font-interSemiBold text-[10px] sm:text-[12px] lg:text-[16px] text-white text-center leading-none mt-1">
+              HORAS
+            </span>
+          </div>
+        </div>
+
+        <div class="flex flex-col justify-center items-center">
+          <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+            :
+          </span>
+        </div>
+
+        <div class="w-[60px] h-[70px] sm:w-[120px] sm:h-[120px] lg:w-[160px] lg:h-[160px] p-[16px] sm:p-[24px] lg:p-[32px] bg-[#67B0C480] rounded-[8px] flex flex-col justify-center items-center flex-shrink-0">
+          <div class="flex flex-col items-center space-y-[8px]">
+            <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+              {{ countdown.minutes }}
+            </span>
+            <span class="font-interSemiBold text-[10px] sm:text-[12px] lg:text-[16px] text-white text-center leading-none mt-1">
+              MIN
+            </span>
+          </div>
+        </div>
+
+        <div class="flex flex-col justify-center items-center">
+          <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+            :
+          </span>
+        </div>
+
+        <div class="w-[60px] h-[70px] sm:w-[120px] sm:h-[120px] lg:w-[160px] lg:h-[160px] p-[16px] sm:p-[24px] lg:p-[32px] bg-[#67B0C480] rounded-[8px] flex flex-col justify-center items-center flex-shrink-0">
+          <div class="flex flex-col items-center space-y-[8px]">
+            <span class="font-interBold text-[24px] sm:text-[48px] lg:text-[72px] text-white text-center leading-none">
+              {{ countdown.seconds }}
+            </span>
+            <span class="font-interSemiBold text-[10px] sm:text-[12px] lg:text-[16px] text-white text-center leading-none mt-1">
+              SEG
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <p class="text-center font-interSemiBold text-[24px] leading-[28px] text-white sm:w-[600px]">
+        PREVENTA OFICIAL EN ECUADOR
+      </p>
+
+      <p class="font-inter text-[14px] sm:text-[16px] text-white font-light text-center whitespace-pre-line sm:max-w-[800px]">
+        Rediseñamos la experiencia, y tú puedes ser parte de ella.<br><br>
+        Para los early adopters, ofrecemos beneficios exclusivos, challenges y sorteos que convierten cada momento al
+        volante en algo único y emocionante. Vive lo que otros solo imaginan y descubre cómo es rediseñar lo
+        convencional.
+      </p>
+
     </div>
+  </div>
 </template>
+
 <script lang="ts" setup>
-// --- LÓGICA DE LA CUENTA REGRESIVA ---
+
 const countdown = ref({
   days: '00',
   hours: '00',
