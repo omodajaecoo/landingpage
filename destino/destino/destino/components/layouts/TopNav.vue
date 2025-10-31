abbe4cd32a54fe54ed3a0a758584c356179130d2
omodajaecoo <omodajaecoo@users.noreply.github.com>
2025-10-31 20:15:32 +0000

    sync: actualización automática desde automotores-front

--- destino/destino/destino/components/layouts/TopNav.vue
+++ destino/destino/destino/components/layouts/TopNav.vue
@@ -217,19 +217,19 @@
           name: 'OMODA C5',
           imgUrl: '/header/c5.png',
           brand: brandList[1],
-          linkUrl: '/c5',
+          linkUrl: '/models/omoda-c5',
         },
         {
           name: 'OMODA E5',
           imgUrl: '/header/e5.png',
           brand: brandList[1],
-          linkUrl: '/e5'
+          linkUrl: '/models/omoda-e5'
         },
         {
           name: 'JAECOO J7',
           imgUrl: '/header/j7.png',
           brand: brandList[0],
-          linkUrl: '/j7'
+          linkUrl: '/models/jaecoo-j7'
         },
         
       ]
---
+++ destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/destino/components/BaseImg.vue
@@ -0,0 +1,25 @@
+<template>
+  <img 
+    :src="imgUrl" 
+    :loading="props.loading" 
+    :alt="props.alt"
+    class="select-none"
+  />
+</template>
+
+<script lang="ts" setup>
+  const props = withDefaults(defineProps<{
+    src: string;
+    loading?: 'lazy' | 'eager';
+    alt?: string;
+  }>(), {
+    src: '',
+    loading: 'lazy',
+    alt: ''
+  })
+  const config = useRuntimeConfig()
+  const imgUrl = computed(() => `${config.public.staticURL}/images${props.src.startsWith('/')?'':'/'}${props.src}`)
+</script>
+<style scoped lang='scss'>
+
+</style>
