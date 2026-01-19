42739e4189f27bc81062229b920555ff84615c93
omodajaecoo <omodajaecoo@users.noreply.github.com>
2026-01-19 16:34:01 +0000

    sync: actualización automática desde automotores-front

---
+++ components/EmbededForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div :class="containerClass" :style="containerStyle">
+    <iframe
+      v-if="url"
+      :src="url"
+      :width="width"
+      :height="height"
+      :frameborder="frameborder"
+      :class="iframeClass"
+      :style="iframeStyle"
+      :allowfullscreen="allowFullscreen"
+      :loading="loading"
+    />
+    <div v-else class="flex items-center justify-center p-8 bg-gray-100 rounded">
+      <p class="text-gray-500">No se ha proporcionado una URL para el formulario</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { StyleValue } from 'vue';
+
+interface Props {
+  url: string;
+  width?: string | number;
+  height?: string | number;
+  frameborder?: string | number;
+  containerClass?: string;
+  containerStyle?: StyleValue;
+  iframeClass?: string;
+  iframeStyle?: StyleValue;
+  allowFullscreen?: boolean;
+  loading?: 'lazy' | 'eager';
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  width: '100%',
+  height: '100%',
+  frameborder: '0',
+  containerClass: '',
+  containerStyle: '',
+  iframeClass: '',
+  iframeStyle: '',
+  allowFullscreen: true,
+  loading: 'lazy'
+});
+</script>
+
+<style scoped>
+iframe {
+  display: block;
+}
+</style>
--- pages/quote.vue
+++ pages/quote.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="flex flex-col items-center w-full pt-[60px] sm:pt-0 bg-[#0D0E0E]">
-    <HomeFormInfo />
-    <HomeLeadForm ref="leadFormRef" id="leadform"/>
+    <EmbededForm 
+      :url="URL_FORM"
+      container-class="w-full flex justify-center px-[10px] pt-[30px] pb-[40px] sm:px-[30px] sm:pt-[100px] bg-gradient-to-b from-[#0D0E0E] to-[#67B0C4]"
+      iframe-class="rounded-[8px] lg:w-[80%] max-w-[1200px] shadow-lg"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-
-const leadFormRef = ref();
-
+const URL_FORM = 'https://ayas-formweb-prd.powerappsportals.com/tools/omoda-jaecoo/form-quote-general';
 
 </script>
 <style lang='scss'>
@@ -17,4 +18,14 @@ const leadFormRef = ref();
   line-height: 21px;
   color: #222222;
 }
+
+iframe{
+  height: 100vh;
+  width: 100vw
+}
+
+iframe::-webkit-scrollbar {
+  display: none;
+}
+
 </style>
